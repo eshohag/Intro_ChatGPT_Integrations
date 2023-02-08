264816f27f12dab0d706762c760004f9a5c2e72a
MD Shohag Mia <eshohag@outlook.com>
2023-02-08 21:27:40 +0600

    Add project files.

---
+++ Intro_ChatGPT_Integrations/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Intro_ChatGPT_Integrations.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Intro_ChatGPT_Integrations.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Intro_ChatGPT_Integrations/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Intro_ChatGPT_Integrations.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
